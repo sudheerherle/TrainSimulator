a4052bffa3512adc3e6248b92924d96a682d2558
Sudheer Herle <ssherle89@gmail.com>
2017-07-25 12:49:02 +0530

    Removed COM port options

--- TrainSimulator/src/trainsimulator/communicationSetting.java
+++ TrainSimulator/src/trainsimulator/communicationSetting.java
@@ -35,10 +35,6 @@ private communicationSetting this_class = this;
 //        com4.setEnabled(false);
         ButtonGroup grp = new ButtonGroup();
         grp.add(usb);
-        grp.add(com1);
-        grp.add(com2);
-        grp.add(com3);
-        grp.add(com4);
         usb.setSelected(true);
         usb.setEnabled(true);  
         if(SharedData.getSingletonObject().connected){
@@ -66,11 +62,6 @@ private communicationSetting this_class = this;
         jPanel4 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         usb = new javax.swing.JRadioButton();
-        jPanel1 = new javax.swing.JPanel();
-        com1 = new javax.swing.JRadioButton();
-        com2 = new javax.swing.JRadioButton();
-        com4 = new javax.swing.JRadioButton();
-        com3 = new javax.swing.JRadioButton();
         jLabel1 = new javax.swing.JLabel();
         LblStatus = new javax.swing.JLabel();
         BtnConnect = new javax.swing.JButton();
@@ -125,76 +116,19 @@ private communicationSetting this_class = this;
                 .addContainerGap(8, Short.MAX_VALUE))
         );
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(resourceMap.getString("jPanel1.border.title"))); // NOI18N
-        jPanel1.setName("jPanel1"); // NOI18N
-
-        com1.setFont(resourceMap.getFont("usb.font")); // NOI18N
-        com1.setText(resourceMap.getString("com1.text")); // NOI18N
-        com1.setName("com1"); // NOI18N
-
-        com2.setFont(resourceMap.getFont("usb.font")); // NOI18N
-        com2.setText(resourceMap.getString("com2.text")); // NOI18N
-        com2.setName("com2"); // NOI18N
-
-        com4.setFont(resourceMap.getFont("usb.font")); // NOI18N
-        com4.setText(resourceMap.getString("com4.text")); // NOI18N
-        com4.setName("com4"); // NOI18N
-
-        com3.setFont(resourceMap.getFont("usb.font")); // NOI18N
-        com3.setText(resourceMap.getString("com3.text")); // NOI18N
-        com3.setName("com3"); // NOI18N
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(com1)
-                    .addComponent(com3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(com2)
-                    .addComponent(com4))
-                .addGap(32, 32, 32))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(com2)
-                        .addGap(18, 18, 18)
-                        .addComponent(com4))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(com1)
-                        .addGap(18, 18, 18)
-                        .addComponent(com3)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-
-        jPanel4Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jPanel1, jPanel2});
-
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(26, 26, 26)
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -219,7 +153,7 @@ private communicationSetting this_class = this;
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
@@ -311,12 +245,7 @@ private communicationSetting this_class = this;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnConnect;
     private javax.swing.JLabel LblStatus;
-    private javax.swing.JRadioButton com1;
-    private javax.swing.JRadioButton com2;
-    private javax.swing.JRadioButton com3;
-    private javax.swing.JRadioButton com4;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
